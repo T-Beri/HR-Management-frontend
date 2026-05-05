fffb3e354e82c6a1683bad649d61d55adce373b5
Tithaye Beri <tithaye.beri@gmail.com>
2026-05-05 10:26:31 +0530

    changed styling in countries

--- HR-Management-Frontend/src/main/java/com/example/HR_Management_Frontend/controller/CountryController.java
+++ HR-Management-Frontend/src/main/java/com/example/HR_Management_Frontend/controller/CountryController.java
@@ -34,7 +34,6 @@ public class CountryController {
     @GetMapping
     public String listCountries(
             @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "") String search,
             Model model) {
 
         Map body     = service.getCountries(page);
@@ -49,7 +48,7 @@ public class CountryController {
         model.addAttribute("countries",   countries);
         model.addAttribute("currentPage", page);
         model.addAttribute("totalPages",  totalPages);
-        model.addAttribute("search",      search);   // keep search term in view
+        
 
         return "country/list";
     }
